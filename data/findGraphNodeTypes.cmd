dde43ac1150f39535a7623bcfa676947cd59003f
Robert <rl78746@yahoo.com>
2017-08-14 15:56:19 -0500

    added python script to find types of nodes

---
+++ data/findGraphNodeTypes.cmd
@@ -0,0 +1,1 @@
+python typeFinder.py json
---
+++ data/typeFinder.py
@@ -0,0 +1,32 @@
+#get absolute filepaths
+import os
+#json parsing
+import json 
+#file pattern matching
+import glob
+#command line arguments
+import sys
+
+
+def filesWithExtensions(folderPath, extensions):
+    files = []
+    for extension in extensions:
+        filenames = [x[x.rfind('\\') + 1:] for x in glob.glob(folderPath + '/*' + extension)]
+        files.extend(filenames)
+    return files
+
+if len(sys.argv) == 2:
+	jsonFileExtension = '.json'
+	jsonFolderPath = os.path.abspath(sys.argv[1])
+	jsonFiles = filesWithExtensions(jsonFolderPath, [jsonFileExtension])
+	typesOfNodes = set()
+	for jsonFile in jsonFiles:
+		with open(jsonFolderPath + '/' + jsonFile) as data_file:    
+			data = json.load(data_file)
+			for node in data['nodes']:
+				if 'type' in node:
+					typesOfNodes.add(node['type'])
+	print(sorted(list(typesOfNodes)))
+	print("Number of types of nodes:", len(typesOfNodes))
+else:
+	sys.stderr.write("Syntax : python %s json_folder\n" % sys.argv[0])
